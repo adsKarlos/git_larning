cb235796ba07190d280158ee4b1e6ac3ee24e80c
asd <eerrzz@yandex.ru>
2022-03-11 19:57:07 +0300

    Added minus function

--- first/nums.py
+++ first/nums.py
@@ -1,2 +1,9 @@
-def plus(a, b): #sdf
+def plus(a, b):  # sdf
     return a + b
+
+
+text = 'new text'
+
+
+def minus(a, b):
+    return a - b
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from first.nums import plus
+from first.nums import plus, minus
 
 
 def print_hi(name):
-    print(f'Hi, {name}') #asd
+    print(f'Hi, {name}')  # asd
 
-#sdfsdf
+
+# sdfsdf
 if __name__ == '__main__':
     print_hi('PyCharm')
     print(plus(2, 3))
-
+    print(minus(6, 1))
